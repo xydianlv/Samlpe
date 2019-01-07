3b6b31e97569cc43e77a496103aa0c77bffeaea4
nanmeng <nanmeng2014@xiaochuankeji.cn>
2019-01-07 21:31:29 +0800

    FIX : 保活

---
+++ app/src/main/java/com/example/wyyu/gitsamlpe/test/live/ActivityLiveTest.java
@@ -0,0 +1,12 @@
+package com.example.wyyu.gitsamlpe.test.live;
+
+import com.example.wyyu.gitsamlpe.framework.activity.ToolbarActivity;
+
+/**
+ * Created by wyyu on 2019/1/7.
+ **/
+
+public class ActivityLiveTest extends ToolbarActivity {
+
+
+}
