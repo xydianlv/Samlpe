8a92c4c1aebc279242c41082568cea19821cb211
nanmeng <nanmeng2014@xiaochuankeji.cn>
2019-05-03 19:29:49 +0800

    FIX : vector 绘制 svg 图形

--- app/src/main/java/com/example/wyyu/gitsamlpe/test/card/ActivityCard.java
+++ app/src/main/java/com/example/wyyu/gitsamlpe/test/card/ActivityCard.java
@@ -3,7 +3,12 @@ package com.example.wyyu.gitsamlpe.test.card;
 import android.os.Bundle;
 import android.view.View;
 import com.example.wyyu.gitsamlpe.R;
+import com.example.wyyu.gitsamlpe.framework.ULog;
 import com.example.wyyu.gitsamlpe.framework.activity.FullScreenActivity;
+import com.example.wyyu.gitsamlpe.util.http.HttpGet;
+import com.example.wyyu.gitsamlpe.util.http.IHttpCallBack;
+import java.io.IOException;
+import okhttp3.Response;
 
 /**
  * Created by wyyu on 2018/1/10.
@@ -31,6 +36,24 @@ public class ActivityCard extends FullScreenActivity {
         findViewById(R.id.bt_stop).setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View view) {
                 animationView.stopMove();
+            }
+        });
+
+        findViewById(R.id.bt_request).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                loadData();
+            }
+        });
+    }
+
+    private void loadData() {
+        HttpGet.asyncGet("http://47.105.199.13/collection/list_all", "", new IHttpCallBack() {
+            @Override public void onFailure(IOException exception) {
+                ULog.show(exception.getMessage());
+            }
+
+            @Override public void onResponse(Response response) {
+                ULog.show(response.message());
             }
         });
     }
