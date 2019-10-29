6e91e9c18ed06aa3a4190de374bbd99e00f31fa5
nanmeng <nanmeng2014@xiaochuankeji.cn>
2019-10-29 16:46:30 +0800

    FIX : 尺寸变更动画

--- app/src/main/java/com/example/wyyu/gitsamlpe/test/multi/custom/binder/CustomBinderA.java
+++ app/src/main/java/com/example/wyyu/gitsamlpe/test/multi/custom/binder/CustomBinderA.java
@@ -4,9 +4,12 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.example.wyyu.gitsamlpe.R;
 import com.example.wyyu.gitsamlpe.test.multi.custom.data.CustomDataA;
+import com.example.wyyu.gitsamlpe.util.UIUtils;
 import com.wyyu.multi.binder.HolderBinder;
 
 /**
@@ -14,8 +17,6 @@ import com.wyyu.multi.binder.HolderBinder;
  **/
 
 public class CustomBinderA extends HolderBinder {
-
-    public static final int UPDATE_TYPE_A = 0;
 
     @NonNull @Override
     protected RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent) {
@@ -37,16 +38,25 @@ public class CustomBinderA extends HolderBinder {
 
         private static final int LAYOUT = R.layout.layout_holder_custom_a;
 
+        private ImageView imageView;
         private TextView textView;
 
         HolderCustomA(@NonNull View itemView) {
             super(itemView);
 
             textView = itemView.findViewById(R.id.item_custom_text);
+            imageView = itemView.findViewById(R.id.item_custom_image);
+
+            LinearLayout.LayoutParams layoutParams =
+                new LinearLayout.LayoutParams(UIUtils.dpToPx(120.0f), UIUtils.dpToPx(120.0f));
+
+            imageView.setOnClickListener(v -> imageView.setLayoutParams(layoutParams));
         }
 
         void cacheValue(CustomDataA dataA) {
             textView.setText(String.valueOf(dataA.index));
+            imageView.setLayoutParams(
+                new LinearLayout.LayoutParams(UIUtils.dpToPx(48.0f), UIUtils.dpToPx(48.0f)));
         }
     }
 }
--- app/src/main/java/com/example/wyyu/gitsamlpe/test/multi/custom/binder/CustomBinderB.java
+++ app/src/main/java/com/example/wyyu/gitsamlpe/test/multi/custom/binder/CustomBinderB.java
@@ -1,17 +1,23 @@
 package com.example.wyyu.gitsamlpe.test.multi.custom.binder;
 
+import android.animation.ValueAnimator;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.example.wyyu.gitsamlpe.R;
 import com.example.wyyu.gitsamlpe.test.multi.custom.data.CustomDataB;
+import com.example.wyyu.gitsamlpe.util.UIUtils;
 import com.wyyu.multi.binder.HolderBinder;
 
 /**
  * Created by wyyu on 2019-09-24.
  **/
+
 public class CustomBinderB extends HolderBinder {
 
     @NonNull @Override
@@ -36,15 +42,38 @@ public class CustomBinderB extends HolderBinder {
         private static final int LAYOUT = R.layout.layout_holder_custom_b;
 
         private TextView textView;
+        private ImageView imageView;
 
         HolderCustomB(@NonNull View itemView) {
             super(itemView);
 
             textView = itemView.findViewById(R.id.item_custom_text);
+            imageView = itemView.findViewById(R.id.item_custom_image);
+
+            imageView.setOnClickListener(v -> startAnim());
+        }
+
+        private void startAnim() {
+
+            float divideSize = 120.0f - 48.0f;
+
+            ValueAnimator animator = ValueAnimator.ofInt(0, 100);
+            animator.setInterpolator(new LinearInterpolator());
+            animator.setDuration(200);
+            animator.addUpdateListener(animation -> {
+                float percent = (int) animation.getAnimatedValue() * 1.0f / 100;
+                int currentSize = UIUtils.dpToPx(percent * divideSize + 48.0f);
+
+                imageView.setLayoutParams(new LinearLayout.LayoutParams(currentSize, currentSize));
+            });
+
+            animator.start();
         }
 
         void cacheValue(CustomDataB dataB) {
             textView.setText(String.valueOf(dataB.index));
+            imageView.setLayoutParams(
+                new LinearLayout.LayoutParams(UIUtils.dpToPx(48.0f), UIUtils.dpToPx(48.0f)));
         }
     }
 }
