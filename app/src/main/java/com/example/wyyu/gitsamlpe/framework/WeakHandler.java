b2f186e045012ac059da45cd8a9ba24fc9504c99
nanmeng <nanmeng2014@xiaochuankeji.cn>
2019-12-25 14:37:49 +0800

    FIX : 子线程绘制Surface

---
+++ app/src/main/java/com/example/wyyu/gitsamlpe/framework/WeakHandler.java
@@ -0,0 +1,7 @@
+package com.example.wyyu.gitsamlpe.framework;
+
+/**
+ * Created by wyyu on 2019-12-25.
+ **/
+public class WeakHandler {
+}
---
+++ app/src/main/java/com/example/wyyu/gitsamlpe/test/svga/SvgAExecutor.java
@@ -0,0 +1,7 @@
+package com.example.wyyu.gitsamlpe.test.svga;
+
+/**
+ * Created by wyyu on 2019-12-25.
+ **/
+public class SvgAExecutor {
+}
