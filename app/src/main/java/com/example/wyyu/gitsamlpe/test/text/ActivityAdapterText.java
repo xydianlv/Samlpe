d23740459452dd8dd1b458e24e47522c1e445bf2
nanmeng <nanmeng2014@xiaochuankeji.cn>
2020-03-24 15:00:11 +0800

    FIX : MID_ELLIPSE

--- app/src/main/java/com/example/wyyu/gitsamlpe/test/text/ActivityAdapterText.java
+++ app/src/main/java/com/example/wyyu/gitsamlpe/test/text/ActivityAdapterText.java
@@ -20,7 +20,7 @@ public class ActivityAdapterText extends ToolbarActivity {
     }
 
     private static final String TEST_TEXT =
-        "因为公司项目需要\n全文收起的功能，一共有232423432种UI所以需要写2个全文收起的控件，之前也是用过一个全文收起的控件，但是因为设计原因，在ListView刷新的时候会闪烁，我估计原因是因为控件本身的设计是需要先让TextView绘制完成，然后获取TextView一共有多少行，再判断是否需要全文收起按钮，如果需要，则吧TextView压缩回最大行数，添加全文按钮，这样就会造成ListView的Item先高后低，所以会发生闪烁，后面我也在网上找了几个，发现和之前的设计都差不多，虽然肯定是有解决了这个问题的控件，但是还是决定自己写了，毕竟找到控件后还需要测试，而现在的项目时间不充分啊（另外欢迎指教如何快速的找到自己需要的控件，有时候在Github上面搜索，都不知道具体该用什么关键字），而且自己写，也是一种锻炼。";
+        "因为公司项目需要全文收起的功能，一共有232423432种UI所以需要写2个全文收起的控件，之前也是用过一个全文收起的控件，但是因为设计原因，在ListView刷新的时候会闪烁，我估计原因是因为控件本身的设计是需要先让TextView绘制完成，然后获取TextView一共有多少行，再判断是否需要全文收起按钮，如果需要，则吧TextView压缩回最大行数，添加全文按钮，这样就会造成ListView的Item先高后低，所以会发生闪烁，后面我也在网上找了几个，发现和之前的设计都差不多，虽然肯定是有解决了这个问题的控件，但是还是决定自己写了，毕竟找到控件后还需要测试，而现在的项目时间不充分啊（另外欢迎指教如何快速的找到自己需要的控件，有时候在Github上面搜索，都不知道具体该用什么关键字），而且自己写，也是一种锻炼。";
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +35,7 @@ public class ActivityAdapterText extends ToolbarActivity {
         showAdapterText();
         showExpandText();
         showEllipseText();
+        showMiddleEllipse();
     }
 
     private void showAdapterText() {
@@ -55,5 +56,12 @@ public class ActivityAdapterText extends ToolbarActivity {
             TextUtils.TruncateAt.END).toString();
         string = string + "…全文";
         textView.setText(string);
+    }
+
+    private void showMiddleEllipse() {
+        TextView textView = findViewById(R.id.img_ellipse_middle_text);
+        textView.setMaxLines(1);
+        textView.setEllipsize(TextUtils.TruncateAt.MIDDLE);
+        textView.setText(TEST_TEXT);
     }
 }
