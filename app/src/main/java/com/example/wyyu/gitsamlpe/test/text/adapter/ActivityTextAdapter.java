283154b490ea5860abacc2056e8f7f4157ac7d63
nanmeng <nanmeng2014@xiaochuankeji.cn>
2020-05-09 20:11:48 +0800

    FIX : 换行文案尾部后缀 全文

--- app/src/main/java/com/example/wyyu/gitsamlpe/test/text/adapter/ActivityTextAdapter.java
+++ app/src/main/java/com/example/wyyu/gitsamlpe/test/text/adapter/ActivityTextAdapter.java
@@ -15,19 +15,22 @@ import com.example.wyyu.gitsamlpe.util.UIUtils;
 
 public class ActivityTextAdapter extends ToolbarActivity {
 
-    public static void open(Context context) {
-        context.startActivity(new Intent(context, ActivityTextAdapter.class));
-    }
-
-    private static final String TEST_TEXT =
-        "因为公司项目需要全文收起的功能，一共有232423432种UI所以需要写2个全文收起的控件，之前也是用过一个全文收起的控件，但是因为设计原因，在ListView刷新的时候会闪烁，我估计原因是因为控件本身的设计是需要先让TextView绘制完成，然后获取TextView一共有多少行，再判断是否需要全文收起按钮，如果需要，则吧TextView压缩回最大行数，添加全文按钮，这样就会造成ListView的Item先高后低，所以会发生闪烁，后面我也在网上找了几个，发现和之前的设计都差不多，虽然肯定是有解决了这个问题的控件，但是还是决定自己写了，毕竟找到控件后还需要测试，而现在的项目时间不充分啊（另外欢迎指教如何快速的找到自己需要的控件，有时候在Github上面搜索，都不知道具体该用什么关键字），而且自己写，也是一种锻炼。";
-
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_adapter);
 
         initActivity();
     }
+
+    public static void open(Context context) {
+        context.startActivity(new Intent(context, ActivityTextAdapter.class));
+    }
+
+    private static final String DIVIDE_TEXT =
+        "因为公司项\n目需要\n全文收起的功能，一共有23242343\n2种UI所以需要写2个全文收起的控\n件，之前也是\n用过一个全文收起的控件，但是因为设计原因";
+
+    private static final String TEST_TEXT =
+        "因为公司项目需要全文收起的功能，一共有232423432种UI所以需要写2个全文收起的控件，之前也是用过一个全文收起的控件，但是因为设计原因，在ListView刷新的时候会闪烁，我估计原因是因为控件本身的设计是需要先让TextView绘制完成，然后获取TextView一共有多少行，再判断是否需要全文收起按钮，如果需要，则吧TextView压缩回最大行数，添加全文按钮，这样就会造成ListView的Item先高后低，所以会发生闪烁，后面我也在网上找了几个，发现和之前的设计都差不多，虽然肯定是有解决了这个问题的控件，但是还是决定自己写了，毕竟找到控件后还需要测试，而现在的项目时间不充分啊（另外欢迎指教如何快速的找到自己需要的控件，有时候在Github上面搜索，都不知道具体该用什么关键字），而且自己写，也是一种锻炼。";
 
     private void initActivity() {
         initToolbar("AdapterText", 0xffffffff, 0xff84919b);
@@ -36,6 +39,7 @@ public class ActivityTextAdapter extends ToolbarActivity {
         showExpandText();
         showEllipseText();
         showMiddleEllipse();
+        showAdjustText();
     }
 
     private void showAdapterText() {
@@ -62,6 +66,12 @@ public class ActivityTextAdapter extends ToolbarActivity {
         TextView textView = findViewById(R.id.img_ellipse_middle_text);
         textView.setMaxLines(1);
         textView.setEllipsize(TextUtils.TruncateAt.MIDDLE);
+        textView.setText(TEST_TEXT);
+    }
+
+    private void showAdjustText() {
+        AdjustTextView textView = findViewById(R.id.adjust_text_test);
+        textView.setMaxLines(4);
         textView.setText(TEST_TEXT);
     }
 }
---
+++ app/src/main/java/com/example/wyyu/gitsamlpe/test/text/adapter/AdjustTextView.java
@@ -0,0 +1,75 @@
+package com.example.wyyu.gitsamlpe.test.text.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.style.ClickableSpan;
+import android.util.AttributeSet;
+import android.view.View;
+import com.example.wyyu.gitsamlpe.framework.toast.UToast;
+
+/**
+ * Created by wyyu on 2020-05-09.
+ **/
+
+public class AdjustTextView extends AppCompatTextView {
+
+    public AdjustTextView(@NonNull Context context) {
+        super(context);
+    }
+
+    public AdjustTextView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AdjustTextView(@NonNull Context context, @Nullable AttributeSet attrs,
+        int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private static final String ELLIPSE = " …全文";
+
+    @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        if (getLineCount() > getMaxLines()) {
+            adaptTextShow();
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
+    private void adaptTextShow() {
+        int textCount = 0;
+        for (int index = 0; index < getMaxLines(); index++) {
+            textCount = textCount + getLayout().getLineEnd(index) - getLayout().getLineStart(index);
+        }
+        String content = getText().toString().substring(0, textCount);
+        if (content.charAt(content.length() - 1) == '\n') {
+            content = content.substring(0, content.length() - 1) + ELLIPSE;
+        } else {
+            content = content.substring(0, content.length() - ELLIPSE.length()) + ELLIPSE;
+        }
+        SpannableString spanString = new SpannableString(content);
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override public void onClick(@NonNull View widget) {
+                UToast.showShort(getContext(), "OnClickEllipse");
+            }
+
+            @Override public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(0xffff0000);
+                ds.setUnderlineText(false);
+                ds.clearShadowLayer();
+            }
+        };
+        spanString.setSpan(clickableSpan, spanString.length() - 3, spanString.length(),
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        setText(spanString);
+    }
+}
