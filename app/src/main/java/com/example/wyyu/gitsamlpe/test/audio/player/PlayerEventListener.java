9e13b83f72370e395f477ba7f711e55f96e4a449
nanmeng <nanmeng2014@xiaochuankeji.cn>
2020-10-16 10:10:48 +0800

    FIX : log

--- app/src/main/java/com/example/wyyu/gitsamlpe/test/audio/player/PlayerEventListener.java
+++ app/src/main/java/com/example/wyyu/gitsamlpe/test/audio/player/PlayerEventListener.java
@@ -1,5 +1,6 @@
 package com.example.wyyu.gitsamlpe.test.audio.player;
 
+import android.util.Log;
 import androidx.annotation.Nullable;
 import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.PlaybackParameters;
@@ -15,43 +16,43 @@ import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 public abstract class PlayerEventListener implements Player.EventListener {
     @Override
     public void onTimelineChanged(Timeline timeline, @Nullable Object manifest, int reason) {
-
+        Log.e("PlayerEventListenerT","onTimelineChanged");
     }
 
     @Override
     public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
-
+        Log.e("PlayerEventListenerT","onTracksChanged");
     }
 
     @Override public void onLoadingChanged(boolean isLoading) {
-
+        Log.e("PlayerEventListenerT","onLoadingChanged");
     }
 
     @Override public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-
+        Log.e("PlayerEventListenerT","onPlayerStateChanged");
     }
 
     @Override public void onRepeatModeChanged(int repeatMode) {
-
+        Log.e("PlayerEventListenerT","onRepeatModeChanged");
     }
 
     @Override public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
-
+        Log.e("PlayerEventListenerT","onShuffleModeEnabledChanged");
     }
 
     @Override public void onPlayerError(ExoPlaybackException error) {
-
+        Log.e("PlayerEventListenerT","onPlayerError");
     }
 
     @Override public void onPositionDiscontinuity(int reason) {
-
+        Log.e("PlayerEventListenerT","onPositionDiscontinuity");
     }
 
     @Override public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
-
+        Log.e("PlayerEventListenerT","onPlaybackParametersChanged");
     }
 
     @Override public void onSeekProcessed() {
-
+        Log.e("PlayerEventListenerT","onSeekProcessed");
     }
 }
