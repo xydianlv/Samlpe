e9949944b619b5b37b23ae4a1d7b74ba138952f9
nanmeng <nanmeng2014@xiaochuankeji.cn>
2022-02-24 21:07:41 +0800

    FIX ：手动设置状态栏色值

--- app/src/main/java/com/example/wyyu/gitsamlpe/test/visibility/ActivityRealFullScreen.java
+++ app/src/main/java/com/example/wyyu/gitsamlpe/test/visibility/ActivityRealFullScreen.java
@@ -5,10 +5,15 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
 import android.view.WindowManager;
+
 import com.example.wyyu.gitsamlpe.R;
+
+import butterknife.BindView;
 
 /**
  * Created by wyyu on 2020-01-14.
@@ -20,11 +25,17 @@ public class ActivityRealFullScreen extends AppCompatActivity {
         context.startActivity(new Intent(context, ActivityRealFullScreen.class));
     }
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.ToolbarActivityTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_real_full_screen);
 
+        setFullScreenShow();
+        initClick();
+    }
+
+    private void setFullScreenShow() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             //getWindow().getDecorView()
@@ -32,11 +43,28 @@ public class ActivityRealFullScreen extends AppCompatActivity {
             //        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
         getWindow().getDecorView()
-            .setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                .setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
+    }
+
+    private void initClick() {
+        View.OnClickListener clickListener = view -> {
+            switch (view.getId()) {
+                case R.id.full_screen_click_a:
+                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                    break;
+                case R.id.full_screen_click_b:
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                    }
+                    break;
+            }
+        };
+        findViewById(R.id.full_screen_click_a).setOnClickListener(clickListener);
+        findViewById(R.id.full_screen_click_b).setOnClickListener(clickListener);
     }
 }
